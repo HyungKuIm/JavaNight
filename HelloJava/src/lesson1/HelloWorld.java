f3991de1b1d2b12b6885ac6a7ca0b4dab87a7bba
A <A@room5-teacher>
2026-01-20 20:35:59 +0900

    First commit

---
+++ HelloJava/src/lesson1/HelloWorld.java
@@ -0,0 +1,16 @@
+package lesson1;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 이건 주석
+		/*
+		 * 이것도 주석
+		 * 항상 주석
+		 */
+		System.out.println("Hello OracleJava");
+		System.out.println("임형구");
+	}
+
+}
