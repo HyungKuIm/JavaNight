2217c06d9008b83094fd97caca13b44e53833ecb
A <A@room5-teacher>
2026-01-27 21:51:24 +0900

    3일차 종료

--- HelloJava/src/lesson3/PersonTest.java
+++ HelloJava/src/lesson3/PersonTest.java
@@ -5,14 +5,14 @@ public class PersonTest {
 	public static void main(String[] args) {
 		Person person = new Person(); // 사람 1명 생성
 		// 사람 인스턴스 생성
-		person.name = "강호동";
-		person.age = 30;
+		person.setName("강호동");
+		person.setAge(30);
 
-		System.out.println("이름:" + person.name);
-		System.out.println("나이:" + person.age);
+		System.out.println("이름:" + person.getName());
+		System.out.println("나이:" + person.getAge());
 
-		// 캡슐화(Encapsulization)
-		//   : 클래스, 접근제어, 레코드, 생성자, 메소드
+		// 캡슐화(Encapsulation)
+		//   : 클래스, 생성자, 접근제어,  메소드, 레코드
 		// 상속성(Inheritance)
 		//   : 부모, 자식과의 관계, 상속한다는 개념
 		// 다형성(PolyMorphism)
