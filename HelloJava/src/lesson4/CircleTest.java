c712a4060d99fa6058752d7217db6ab6f3537401
A <A@room5-teacher>
2026-01-28 21:50:42 +0900

    4일차 수업내용

---
+++ HelloJava/src/lesson4/CircleTest.java
@@ -0,0 +1,11 @@
+package lesson4;
+
+public class CircleTest {
+
+	public static void main(String[] args) {
+		Circle circle = new Circle(2);
+		circle.showArea();
+
+	}
+
+}
---
+++ HelloJava/src/lesson4/InheChild.java
@@ -0,0 +1,8 @@
+package lesson4;
+
+public class InheChild extends Inhe {
+
+	public InheChild() {
+		System.out.println("자식 클래스입니다.ㅠㅠ");
+	}
+}
