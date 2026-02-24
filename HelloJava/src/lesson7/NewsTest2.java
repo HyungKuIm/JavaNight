9a63ed76b8516a3e21065d92610dd8aae77f4aa5
A <A@room5-teacher>
2026-02-24 19:07:08 +0900

    8일차까지 커밋

---
+++ HelloJava/src/lesson7/NewsTest2.java
@@ -0,0 +1,30 @@
+package lesson7;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class NewsTest2 {
+
+	public static void main(String[] args) {
+		Path path = Paths.get("C:\\Users\\A\\Documents\\news.txt");
+
+		try(BufferedReader reader =
+				Files.newBufferedReader(path, StandardCharsets.UTF_8)) {
+			for (String line; (line = reader.readLine()) != null;) {
+				System.out.println(line);
+			}
+		} catch (IOException ex) {
+			System.out.println(ex);
+		}
+
+	}
+
+}
