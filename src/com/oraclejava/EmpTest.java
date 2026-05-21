4ba51869e2291a40a97df2ea33ac87fb32bf5c96
mac <damansa1@naver.com>
2026-05-21 11:50:34 +0900

    인터페이스 실습

--- src/com/oraclejava/EmpTest.java
+++ src/com/oraclejava/EmpTest.java
@@ -1,65 +1,73 @@
 package com.oraclejava;
 
-abstract class Emp {
-    private String empno;
-    private String ename;
+/**
+ * 사원이 되려면 출근하고, 일하고, 퇴근해야 한다는 규칙, 계약 사항
+ * @author HyungKu Im
+ */
+interface Emp {
+    void gotoWork();
+    void work();
+    void getOffWork();
+}
 
-    public void gotoWork() {
-        System.out.println(ename + " 출근합니다.");
-    }
+class Programmer implements Emp {
+    String ename;
 
-    abstract void work();
-
-    public void getOffWork() {
-        System.out.println(ename + " 퇴근합니다.");
-    }
-
-    public String getEmpno() {
-        return empno;
-    }
-
-    public void setEmpno(String empno) {
-        this.empno = empno;
-    }
-
-    public String getEname() {
-        return ename;
-    }
-
-    public void setEname(String ename) {
+    public Programmer(String ename) {
         this.ename = ename;
     }
-}
-
-class Programmer extends Emp {
 
     @Override
-    void work() {
-        System.out.println(getEname() + "이(가) 개발을 합니다");
+    public void gotoWork() {
+        System.out.println(ename + " 사원이 자전거로 출근합니다.");
+    }
+
+    @Override
+    public void work() {
+        System.out.println(ename + " 사원이 개발합니다.");
+    }
+
+    @Override
+    public void getOffWork() {
+        System.out.println(ename + " 사원이 자전거로 퇴근합니다.");
     }
 }
 
-class Designer extends Emp {
+class Designer implements Emp {
+
+    String ename;
+    public Designer(String ename) {
+        this.ename = ename;
+    }
+
     @Override
-    void work() {
-        System.out.println(getEname() + "이(가) 디자인을 합니다.");
+    public void gotoWork() {
+        System.out.println(ename + " 사원이 지하철로 출근합니다.");
+    }
+
+    @Override
+    public void work() {
+        System.out.println(ename + " 사원이 디자인합니다.");
+    }
+
+    @Override
+    public void getOffWork() {
+        System.out.println(ename + " 사원이 지하철로 퇴근합니다.");
     }
 }
 
 public class EmpTest {
     public static void main(String[] args) {
-        Programmer p = new Programmer();
-        p.setEmpno("1111");
-        p.setEname("홍길동");
+        Emp p = new Programmer("홍길동");
         p.gotoWork();
         p.work();
         p.getOffWork();
-        Designer d = new Designer();
-        d.setEmpno("2222");
-        d.setEname("홍길순");
+
+        Emp d= new Designer("카리나");
         d.gotoWork();
         d.work();
         d.getOffWork();
+
 
     }
 }
