19772503e3fdc1f1c6b983b229c946021de3f0fe
mac <damansa1@naver.com>
2026-05-21 11:08:30 +0900

    상속 실습

--- src/com/oraclejava/EmpTest.java
+++ src/com/oraclejava/EmpTest.java
@@ -2,24 +2,30 @@ package com.oraclejava;
 
 class Emp {
     String ename;
-    public Emp() {
-        this("홍길동");
+    public void gotoWork() {
+        System.out.println(ename + " 출근");
     }
 
-    public Emp(String ename) {
+}
+
+class Programmer extends Emp {
+    public Programmer(String ename) {
         this.ename = ename;
     }
 
-    public String toString() {
-        return "[EMP : ename = " + ename + "]";
+    public void work() {
+        System.out.println(ename + "이 개발합니다.");
     }
 }
 
 public class EmpTest {
     public static void main(String[] args) {
-        Emp e = new Emp();
-        System.out.println(e);
-        Emp e1 = new Emp("홍길순");
-        System.out.println(e1);
+//        Emp e = new Emp();
+//        System.out.println(e);
+        Programmer p = new Programmer("홍길동");
+        p.gotoWork();
+        p.work();
+//        Emp e1 = new Emp("홍길순");
+//        System.out.println(e1);
     }
 }
